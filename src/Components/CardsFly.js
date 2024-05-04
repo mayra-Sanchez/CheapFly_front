e290b6455c6e92a730380c8a5d00694d8663a4a5
Mayra Sanchez <mayra.alejandra.sanchez@correounivalle.edu.co>
2024-05-04 11:52:18 -0500

    add cardfly component

---
+++ src/Components/CardsFly.js
@@ -0,0 +1,18 @@
+import "./CardFly.css";
+import { Card } from 'primereact/card';
+
+function CardsFly() {
+    return (
+        <div className="CardFly">
+            <Card title="Simple Card">
+                <p className="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+                    numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
+                </p>
+            </Card>
+        </div>
+
+    );
+}
+
+export default CardsFly;
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,9 +1,23 @@
+import React from 'react';
 import "./Search.css";
+import CardsFly from "./CardsFly";
 
 function Search() {
-    return(
+
+    return (
         <div className="container-search">
-            
+            {/* <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                    <Lottie
+                        options={{
+                            loop: true,
+                            autoplay: true,
+                            animationData: animationData,
+                        }}
+                        height={200}
+                        width={200}
+                    />
+                </div> */}
+            <CardsFly />
         </div>
     );
 }
